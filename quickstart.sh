5568696c85e3b3e14795eeaf2cada9f5e3446e9a
Michael V. DePalatis <mike@depalatis.net>
2018-06-11 17:26:30 -0400

    Update to use modern Python and conda 4.5

--- quickstart.sh
+++ quickstart.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-curl https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -o miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
+curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
+bash miniconda.sh -b
+echo 'source $HOME/miniconda3/bin/activate' >> $HOME/.bashrc
+source $HOME/.bashrc
+conda activate
 conda install -y numpy scipy pandas matplotlib swig cmake
 conda install -y -c pennmem ptsa
